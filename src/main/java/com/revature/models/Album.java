636edcc9a25282a9d5414a3c5557455ebb4c1c5c
Neil Crum <nhc.crum@outlook.com>
2024-04-13 10:56:33 -0700

    added skeleton for album

--- src/main/java/com/revature/models/Album.java
+++ src/main/java/com/revature/models/Album.java
@@ -1,4 +1,14 @@
 package com.revature.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Component;
+
+//@Entity
+//@Table(name = "albums")
+//@Component
 public class Album {
+
+
+
 }
