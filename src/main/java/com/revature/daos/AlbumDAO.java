9367eaf2d2b2960b225d9154f4c31f5f9679914f
Neil Crum <nhc.crum@outlook.com>
2024-04-14 11:06:55 -0700

    added @Repository annotation and custom findByUserUserId method

--- src/main/java/com/revature/daos/AlbumDAO.java
+++ src/main/java/com/revature/daos/AlbumDAO.java
@@ -2,10 +2,14 @@ package com.revature.daos;
 
 import com.revature.models.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AlbumDAO extends JpaRepository<Album, Integer> {
 
-
-    //public List<Album> albums getAlbumsByUserId(int userid);
+    public List<Album> findByUserUserId(int userId);
+    //public List<Album> getAlbumsByUserId(int userid);
 
 }
