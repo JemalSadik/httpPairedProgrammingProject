3f903240c88a73fa7c1748dcecbec6d080e46fbe
Neil Crum <nhc.crum@outlook.com>
2024-04-14 11:06:23 -0700

    added user dao dependency injection, POST album and get albums by userID,

--- src/main/java/com/revature/controllers/AlbumController.java
+++ src/main/java/com/revature/controllers/AlbumController.java
@@ -1,7 +1,9 @@
 package com.revature.controllers;
 
 import com.revature.daos.AlbumDAO;
+import com.revature.daos.UserDAO;
 import com.revature.models.Album;
+import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,21 +16,25 @@ import java.util.Optional;
 public class AlbumController {
 
     AlbumDAO albumDAO;
+    UserDAO userDAO;
 
     @Autowired
-    public AlbumController(AlbumDAO albumDAO) {
+    public AlbumController(AlbumDAO albumDAO, UserDAO userDAO) {
         this.albumDAO = albumDAO;
+        this.userDAO = userDAO;
     }
 
     // add an album
-    @PostMapping
-    public ResponseEntity<Album> addAlbum(@RequestBody Album album) {
-        Album a = albumDAO.save(album);
-
-        if (a == null) {
-            return ResponseEntity.internalServerError().build();
+    @PostMapping("/{userId}")
+    public ResponseEntity<Album> addAlbum(@RequestBody Album album, @PathVariable int userId) {
+        Optional<User> checkUser = userDAO.findById(userId);
+        if (checkUser.isPresent()) {
+            User user = checkUser.get();
+            album.setUser(user);
+            Album a = albumDAO.save(album);
+            return ResponseEntity.status(201).body(a);
         }
-        return ResponseEntity.status(201).body(a);
+        return ResponseEntity.internalServerError().build();
     }
 
     // update an album by id
@@ -55,33 +61,33 @@ public class AlbumController {
     }
 
     // get an Album by id
-    /*
-    @GetMapping("/{Id}")
+    @GetMapping("/{albumId}")
     public ResponseEntity<Album> getAlbumById(@PathVariable int albumId) {
         if (albumId > 0) {
             Optional<Album> a = albumDAO.findById(albumId);
             if (a.isEmpty()) {
                 return ResponseEntity.internalServerError().build();
             }
-            return ResponseEntity.ok(a);
+            return ResponseEntity.ok(a.get());
         }
         return ResponseEntity.notFound().build();
     }
-    */
+
 
 
     // get albums by userid
-    /*
-    @GetMapping("/{authors/userid}")
-    public ResponseEntity<List<Album>> getAlbumsByUserId(@PathVariable int userid) {
-        List<Album> albums = albumDAO.findBbUserId(userid);
 
-        if (albums == null) {
-            return ResponseEntity.internalServerError().build();
+    //@GetMapping("/{authors/userid}")
+    @GetMapping("/{userid}/albums")
+    public ResponseEntity<List<Album>> getAlbumsByUserId(@PathVariable int userid) {
+        List<Album> albums = albumDAO.findByUserUserId(userid);
+
+        if (albums.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.status(201).body(albums);
+        return ResponseEntity.ok(albums);
     }
-    */
+
     // delete album by id
     @DeleteMapping("/{albumId}")
     public ResponseEntity<String> deleteAlbum(@PathVariable int albumId) {
